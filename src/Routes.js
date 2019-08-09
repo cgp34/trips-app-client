0eed04fc45bd2c7532b8e139da61d038aa96a8e0
christopher.petrunik <christopher.petrunik@workday.com>
2019-08-09 15:46:06 -0400

    uncomment new note refs in routes file

--- src/Routes.js
+++ src/Routes.js
@@ -4,7 +4,7 @@ import Home from "./containers/Home";
 import Notes from "./containers/Notes";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
-/*import NewNote from "./containers/NewNote";*/
+import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
@@ -17,7 +17,7 @@ export default ({ childProps }) =>
     <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
     <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
     <AuthenticatedRoute path="/trips/new" exact component={NewTrip} props={childProps} />    
-/*    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />*/
+    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
     <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
